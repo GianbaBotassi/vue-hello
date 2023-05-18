dffdf8aaa45a602e00a8a2f69b8982a1081ff04a
GianbaBotassi <gianba89@gmail.com>
2023-05-18 15:16:16 +0200

    Done bonus, add bottom that change class to p and img elements

--- js/main.js
+++ js/main.js
@@ -9,8 +9,22 @@ const { createApp } = Vue
     data() {
       return {
         welcome: 'Hello Vue',
-        // message: 'if you click here, there is a surprise',
-        // button : 'Click me'
+        message: 'If you are ready to fight, click the blue bottom!',
+        button :'Click me',
+        img: 'img/vue_js_vs._react_js.png',
+        classeImg: 'none',
+        classeP: ''
       }
-    }
+    },
+    methods: {
+        changeImg : function(){
+            if(this.classeImg === 'none' && this.classeP === ''){
+                this.classeImg = '';
+                this.classeP = 'none'
+            }else{
+                this.classeImg = 'none';
+                this.classeP = ''
+            }
+        }
+      }
   }).mount('#app')
